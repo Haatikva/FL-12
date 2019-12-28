dcb03d088e9c697b71fdd890b56ac2d83ba04430
Leonid Novatchynskyi <39277594+Haatikva@users.noreply.github.com>
2019-12-28 19:27:28 +0200

    Update task2.js

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,53 +1,56 @@
 // Your code goes here
-const constAttmp = 3,
-    constMult = 8,
-    constGain = 100,
-    constTotLoose = 0;
-
-let multiply = 8,
-    choosedNumb, 
-    attmp = 3, 
-    curPrize=100,
-    winPerRound=0,
-    totWin=0,
-    a=true;
+const constAttmp = 3;
+const constMult = 8;
+const constGain = 100;
+const constTotLoose = 0;
+let multiply = 8;
+let choosedNumb;
+let attmp = 3;
+let curPrize = 100;
+let totWin = 0;
+let winPerRound = 0;
+let a = true;
+let rnumb;
+let multiplyAddNumb=4;
+let MaxAttmpt = 3;
+let GainMultOrDiv = 2;
 
     a=confirm('Do you want to play a game?');
-if(!a){
-    alert('You did not become a billionaire, but can.');
-}else {
-    do {
-            let rnumb = Math.round(Math.random()*multiply);
-            console.log(rnumb);
-                
-            for (let attmpleft=0; attmpleft<3; attmpleft++) {
-                choosedNumb = parseInt(prompt(`Choose a roulette pocket number from 0 to ${multiply};
+if (!a) {
+  alert('You did not become a billionaire, but can.');
+} else {
+  do {
+    rnumb = Math.round(Math.random() * multiply);
+    for (let attmpleft = 0; attmpleft < MaxAttmpt; attmpleft++) {
+      choosedNumb = parseInt(
+        prompt(`Choose a roulette pocket number from 0 to ${multiply};
 Attempts left: ${attmp}
 Total prize: ${totWin}$;
-Possible prize on current attempt: ${curPrize}$`));
-                if (rnumb === choosedNumb) {
-                    winPerRound=curPrize;
-                    totWin+=curPrize;
-                    curPrize = constGain*2;
-                    attmp = constAttmp;
-                    multiply+=4;
-                    break;
-                }else {
-                     curPrize/=2;
-                       attmp--;
-               }
-           }
-
-                if (choosedNumb !== rnumb) {
-                alert(`Thank you for your participation. Your prize is: ${totWin}$`);
-                a = confirm('Do you want to play a game?');
-                attmp = constAttmp;
-                totWin = constTotLoose;
-                curPrize = constGain;
-                multiply = constMult;
-                }else if (choosedNumb === rnumb) {
-                a = confirm(`Congratulation, you won! Your prize is: ${winPerRound}$. Do you want to continue?`);
-                }
-    }   
-while(a)
+Possible prize on current attempt: ${curPrize}$`)
+      );
+      if (rnumb === choosedNumb) {
+        winPerRound = curPrize;
+        totWin += curPrize;
+        curPrize = constGain * GainMultOrDiv;
+        attmp = constAttmp;
+        multiply +=multiplyAddNumb;
+        break;
+      } else {
+        curPrize /= GainMultOrDiv;
+        attmp--;
+      }
+    }
+    if (choosedNumb !== rnumb) {
+      alert(`Thank you for your participation. Your prize is: ${totWin}$`);
+      a = confirm('Do you want to play a game?');
+      attmp = constAttmp;
+      totWin = constTotLoose;
+      curPrize = constGain;
+      multiply = constMult;
+    } else if (choosedNumb === rnumb) {
+      a = confirm(
+        `Congratulation, you won! Your prize is: ${winPerRound}$. Do you want to continue?`
+      );
+    }
+  } while (a);
 }
